afe747d4550a41fb6071b6c9370dea7177cd6bdd
El Mahdaoui Alae <alaeelmahdaoui01@gmail.com>
2025-11-02 20:46:11 +0100

    problem5

--- src/problem5/BookStack.java
+++ src/problem5/BookStack.java
@@ -6,21 +6,25 @@ public class BookStack {
     private LinkedList<Book> stack;
 
     public BookStack() {
-
+        stack = new LinkedList<>() ;
     }
 
     public void push(Book book) {
-
+        stack.addFirst(book);
     }
 
     public Book pop() {
-        // code here and don't to test if it's empty
-        return null ;
+        if (stack.isEmpty()){
+            return null;
+        }
+        return stack.removeFirst();
     }
 
     public Book peek() {
-        // code here and don't to test if it's empty
-        return null ;
+        if (stack.isEmpty()){
+            return null;
+        }
+        return stack.getFirst();
     }
 
     public boolean isEmpty() {
@@ -29,7 +33,9 @@ public class BookStack {
 
     public void display() {
         System.out.println("Stack contents (top to bottom):");
-
+        for (Book book : stack) {
+            System.out.println("- " + book);
+        }
     }
 }
 
